7ad3a09492f14c81f5bb51cdc638b82c7f837a16
Marten <martenhz>
2017-07-02 00:05:51 +0200

    Keeping track of already visited links and found links and added filtering for found links

--- scraper.py
+++ scraper.py
@@ -9,6 +9,10 @@ START_WEB_PAGE = config["START_WEB_PAGE"]
 RELEVANT_URI = config["RELEVANT_URI"]
 
 
+found_links = []
+visited_links = []
+
+
 # Get all the links on the page
 def get_all_anchors(webpage):
     page = requests.get(START_DOMAIN + webpage)
@@ -16,8 +20,8 @@ def get_all_anchors(webpage):
     return tree.xpath('//a/@href')
 
 # Check if the link is relevant
-def is_relevant_link(x):
-    return x.startswith(RELEVANT_URI)
+def is_relevant_link(link):
+    return link.startswith(RELEVANT_URI) and link not in found_links
 
 # Remove non-relevant links
 def filter_relevant_links(anchors):
@@ -25,22 +29,40 @@ def filter_relevant_links(anchors):
 
 # Gather relevant links from a relevant web page
 def gather_relevant_links(link):
-    return filter_relevant_links(get_all_anchors(link))
+    if link not in visited_links:
+        # print "Gathering relevant links for link:", link
+        relevant_links = filter_relevant_links(get_all_anchors(link))
+        visited_links.append(link)
+        found_links.append(relevant_links)
+        return relevant_links
+    # print "Link %s has already been visited, skipping..." % link
+    return []
 
+def gather_relevant_links_recursively(link):
+    relevant_links = gather_relevant_links(link)        
+    for relevant_link in relevant_links:
+        gather_relevant_links(relevant_link)
 
-# Remove all links which do not belong to the current domain
+def is_interesting_found_link(link):
+    return ":" not in link
+
+def filter_found_links(found_links):
+    return filter(lambda x: is_interesting_found_link(x), found_links)
+        
+
+# Get all links from the start page
 relevant_start_links = gather_relevant_links(START_WEB_PAGE)
 
-pool = ThreadPool(10)
+print "Started gathering links from %d start links" % len(relevant_start_links)
 
-
-relevant_links = pool.map(gather_relevant_links, relevant_start_links)
-
-# keeping track of already visited links
-
-print "Relevant links: ", relevant_links
-
+pool = ThreadPool(8)
+pool.map(gather_relevant_links_recursively, relevant_start_links)
 
 # close the pool and wait for the work to finish 
 pool.close() 
 pool.join() 
+
+filtered_found_links = filter_found_links(found_links)
+
+#print "Found %d links: %s" % (len(found_links), found_links)
+print "Found %d links" % len(found_links)
